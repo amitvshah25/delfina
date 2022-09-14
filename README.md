025d5dc550bb5be83cd1c5ce43831d295d6bc4cd
Amit Shah <amit@unknown86cc9c8a698b.attlocal.net>
2022-09-14 15:59:34 -0400

    tests, documentation and cleanup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# delfina
+# delfina project
+
+## Start Web Project
+`docker-compose up --build`
+
+## Open browser with URL `http://localhost:8080` to see random dolphin image
+
+## Run tests
+`docker exec -it amit-delfina-web pytest`
--- app/test_main.py
+++ app/test_main.py
@@ -3,7 +3,12 @@ from .main import app
 
 client = TestClient(app)
 
-def test_picture():
-    response = client.get("/picture")
-    assert response.status_code == 200
-    assert response.headers.get("content-type") == "image/jpg"
+def test_ratelimit():
+    response_list = []
+    for req_count in range(0, 5): # make 5 requests to test 200 and 429 statuses
+        response_list.append(client.get("/picture"))
+    assert response_list[0].status_code == 200 # first response should be success
+    assert response_list[0].headers.get("content-type") == "image/jpg" # getting a jpg
+    response_codes = map(lambda r: r.status_code, response_list[1:]) # subsequent responses should be 429
+    assert list(response_codes) == [429]*4
+
